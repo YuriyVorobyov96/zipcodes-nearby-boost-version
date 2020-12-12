19415d9c87b6f3cde7fe865816fa5c76f2efd8dd
YuriyVorobyov96 <darthrevan1333@gmail.com>
2020-12-12 12:05:29 +0300

    v1

---
+++ README.md
@@ -0,0 +1,64 @@
+[![NPM Downloads](https://img.shields.io/npm/dt/zipcodes-nearby-boost.svg?style=for-the-badge)](https://www.npmjs.com/package/zipcodes-nearby-boost)
+[![NPM Version](https://img.shields.io/npm/v/zipcodes-nearby-boost.svg?style=for-the-badge)](https://www.npmjs.com/package/zipcodes-nearby-boost)
+[![License](https://img.shields.io/github/license/YuriyVorobyov96/zipcodes-nearby-boost-version.svg?style=for-the-badge)](https://github.com/YuriyVorobyov96/zipcodes-nearby-boost-version/blob/master/LICENSE)
+
+# Zipcodes near by (Boosted version)
+
+Find a list of zipcodes within a certain distance of a given zipcode.
+
+## Installation
+
+    npm install zipcodes-nearby-boost
+
+## Usage
+
+The method `near(zipcode|coordinates, distance[, options])` takes 2 to 3 arguments.
+
+* First is the zipcode or geo-coordinates (long/lat) to search around
+* Second is the distance (in meters) to search within
+* Third is an optional object with the following attributes:
+  * `datafile`: name for the CSV file containing zipcodes coordinates. By default it looks for a file `zipcodes.csv` in the same folder
+  * `zipcode`: name of the column in the CSV file containing the zipcode. By default it is `Zipcode`
+  * `long`: name of the column in the CSV file containing the zipcode longitude. By default it is `Long`
+  * `lat`: name of the column in the CSV file containing the zipcode latitude. By default it is 'Lat'
+
+For examples:
+
+```
+const zipcodes = require('zipcodes-nearby-boost');
+
+// find zipcodes within 10km from 95020 using the default datafile "zipcodes.csv"
+nearby = await zipcodes.near('95020', 10000);
+
+// find zipcodes within 10km from a location given by geo-coordinates using the default datafile "zipcodes.csv"
+nearby = await zipcodes.near({ longitude: 78.8, latitude: 48.3 }, 10000);
+
+```
+
+## Data file
+
+The data file with zipcodes coordinates must be supplied.  We have include one
+for USA to use.  It can be replaced by zipcodes (or postal codes) from any country
+as long as the file contains the zipcode, the longitude, and the latitude.
+
+You can download an updated file for the USA at <http://federalgovernmentzipcodes.us/>
+
+
+## License
+
+[MIT](https://github.com/YuriyVorobyov96/zipcodes-nearby-boost-version/blob/master/LICENSE)
+
+
+## Author Information
+
+Quoc Vu  
+
+* https://linkedin.com/in/quocvu  
+* https://github.com/quocvu
+
+## Boost Version Author Information
+
+Vorobyov Yuriy  
+
+* https://linkedin.com/in/yuriy-vorobyov-b27b871a8/
+* https://github.com/yuriyvorobyov96
---
+++ index.js
@@ -0,0 +1,105 @@
+const path = require('path');
+
+const CONVERT = 1609.34;
+
+const initGeo = async(filepath, options) => {
+  const Csv = require('fast-csv');
+  const geolib = require('geolib');
+  const _ = require('lodash');
+
+  if (!filepath) {
+    throw new Error('Cannot find file');
+  }
+
+  const csvOptions = {
+    discardUnmappedColumns: true,
+    headers: true,
+    ignoreEmpty: true,
+    trim: true,
+  };
+
+  const parsedDataFile = await new Promise(resolve => {
+    const columns = {
+      long: options && options.long !== undefined ? options.long : 'Long',
+      lat: options && options.lat !== undefined ? options.lat : 'Lat',
+      zipcode: options && options.zipcode !== undefined ? options.zipcode : 'Zipcode',
+    };
+
+    const resultRows = [];
+
+    Csv.fromPath(filepath, csvOptions)
+      .validate(data => data
+          && _.has(data, columns.long) && !_.isEmpty(data[columns.long])
+          && _.has(data, columns.lat) && !_.isEmpty(data[columns.lat])
+          && _.has(data, columns.zipcode) && !_.isEmpty(data[columns.zipcode]))
+      .on('data', row => {
+        resultRows.push(row);
+      })
+      .on('end', () => {
+        resolve(resultRows);
+      });
+  });
+
+  const findCoordinates = (zipcode, columns) => {
+    const index = parsedDataFile.findIndex(el => zipcode === el[columns.zipcode]);
+
+    if (index === -1) {
+      throw new Error(`Cannot find zipcode ${zipcode}`);
+    }
+
+    const found = parsedDataFile[index];
+
+    return {
+      latitude: found[columns.lat],
+      longitude: found[columns.long],
+    };
+  };
+
+  const findNear = (center, radius, columns) => parsedDataFile
+    .filter(el => {
+      const distance = geolib.getDistance(
+        center,
+        { latitude: el[columns.lat], longitude: el[columns.long] },
+      );
+
+      return distance <= radius;
+    })
+    .map(location => location[columns.zipcode]);
+
+  return {
+    near(origin, distance) {
+      const columns = {
+        long: options && options.long !== undefined ? options.long : 'Long',
+        lat: options && options.lat !== undefined ? options.lat : 'Lat',
+        zipcode: options && options.zipcode !== undefined ? options.zipcode : 'Zipcode',
+      };
+
+      if (_.isObject(origin) && _.has(origin, 'longitude') && _.has(origin, 'latitude')) {
+        return findNear(origin, distance, columns);
+      }
+
+      const center = findCoordinates(origin, columns);
+
+      return findNear(center, distance, columns);
+    },
+  };
+};
+
+let geo;
+
+initGeo(path.join(__dirname, './zipcodes.csv'))
+  .then(created => {
+    geo = created;
+  });
+
+module.exports = {
+  async near(zip, area) {
+    if (!geo) {
+      geo = await initGeo(path.join(__dirname, './zipcodes.csv'))  
+    }
+
+    const nearby = await geo.near(zip, area);
+
+    return nearby;
+  },
+};
